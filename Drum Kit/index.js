743c11a5abe830e95b3be600d10da1d2ad714188
Yash Mistry <76916876+yashms25@users.noreply.github.com>
2022-10-10 09:59:28 +0530

    Added drum kit

---
+++ Drum Kit/index.js
@@ -0,0 +1,61 @@
+var totalDrum = document.querySelectorAll(".drum").length;
+
+for (let index = 0; index < totalDrum; index++) {
+  document
+    .querySelectorAll("button")
+    [index].addEventListener("click", function () {
+      var buttonInnerhtml = this.innerHTML;
+      makeSound(buttonInnerhtml);
+      buttonAnimation(buttonInnerhtml);
+    });
+}
+
+document.addEventListener("keydown", function (event) {
+  makeSound(event.key);
+  buttonAnimation(event.key);
+});
+
+function makeSound(key) {
+  switch (key) {
+    case "w":
+      var tom1 = new Audio("sounds/tom-1.mp3");
+      tom1.play();
+      break;
+    case "a":
+      var tom2 = new Audio("sounds/tom-2.mp3");
+      tom2.play();
+      break;
+    case "s":
+      var tom3 = new Audio("sounds/tom-3.mp3");
+      tom3.play();
+      break;
+    case "d":
+      var tom4 = new Audio("sounds/tom-4.mp3");
+      tom4.play();
+      break;
+    case "j":
+      var snare = new Audio("sounds/snare.mp3");
+      snare.play();
+      break;
+    case "k":
+      var crash = new Audio("sounds/crash.mp3");
+      crash.play();
+      break;
+    case "l":
+      var kick = new Audio("sounds/kick-bass.mp3");
+      kick.play();
+      break;
+
+    default:
+      console.log("error button");
+      break;
+  }
+}
+
+function buttonAnimation(key) {
+  var activeButton = document.querySelector("." + key);
+  activeButton.classList.add("pressed");
+  setTimeout(() => {
+    activeButton.classList.remove("pressed");
+  }, 100);
+}
